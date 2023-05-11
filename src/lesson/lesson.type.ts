fe8c0a889f62b93888fe476eb8c8290ccea530f3
Rodrigo Castro <castro.rl@gmail.com>
2023-05-11 16:26:51 -0300

    Created my first graphql resolver and updated a few other files names and folder struccture.

--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -1,7 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 
 @ObjectType('Lesson') //argument to define the name of the class instead of LessonTypeTs
-export class LessonTypeTs {
+export class LessonType {
   @Field(type => ID) //to detine this field as ID field for graphql
   id: string;
 
--- src/lesson/lesson.type.ts/lesson.type.ts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LessonTypeTs } from './lesson.type.ts';
-
-describe('LessonTypeTs', () => {
-  it('should be defined', () => {
-    expect(new LessonTypeTs()).toBeDefined();
-  });
-});
