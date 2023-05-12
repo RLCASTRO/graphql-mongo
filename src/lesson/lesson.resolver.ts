91cbc7e0f5b810df940c94b60f3ddc6055965507
Rodrigo Castro <castro.rl@gmail.com>
2023-05-12 15:02:59 -0300

    Added packages class-validator and class-transformer to validate the lesson input. Created a lesson input DTO to type the lesson input

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,23 +1,18 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
-    @Query(returns => LessonType)
-  lesson(
-    @Args('id') id: string,
-  ) {
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
   }
 
   @Mutation((returns) => LessonType)
-  createLesson(
-    @Args('name') name: string,
-    @Args('startDate') startDate: string,
-    @Args('endDate') endDate: string,
-  ) {
-    return this.lessonService.createLesson(name, startDate, endDate);
+  createLesson(@Args('createLessonInput') createLessonInput: CreateLessonInput) {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
